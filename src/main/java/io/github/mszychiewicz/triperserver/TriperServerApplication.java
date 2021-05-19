8bb41b586ca343482144b464319516a6c204810f
Mateusz Szychiewicz <szychiewiczmateusz@gmail.com>
2021-05-19 23:39:49 +0200

    init

---
+++ src/main/java/io/github/mszychiewicz/triperserver/TriperServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.mszychiewicz.triperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriperServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriperServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/mszychiewicz/triperserver/TriperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mszychiewicz.triperserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriperServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
